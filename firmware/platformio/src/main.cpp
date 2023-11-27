c21906dde3551f3f156ab97224122fa1b275e31f
seth-sievers <ses3wn@umsystem.edu>
2023-11-27 11:47:10 -0600

    work on platformio configuration

--- firmware/platformio/src/main.cpp
+++ firmware/platformio/src/main.cpp
@@ -1,7 +1,12 @@
-#include <avr/io.h>
+//#include <avr/io.h>
+#include <stdint.h>
 
 int main()
 {
+        int8_t signed_integer = -25; 
+        uint8_t unsigned_integer = 0;
+        unsigned_integer = signed_integer;
+        
         while (1) {;}
         return 0;
 }
