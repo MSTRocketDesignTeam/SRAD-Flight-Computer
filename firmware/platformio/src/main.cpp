cf0806e2a76c3b52eac9d5ec40af8e96db3b6b0b
seth-sievers <ses3wn@umsystem.edu>
2023-11-27 12:30:15 -0600

    changed env name

--- firmware/platformio/src/main.cpp
+++ firmware/platformio/src/main.cpp
@@ -3,10 +3,9 @@
 
 int main()
 {
-        int8_t signed_integer = -25; 
-        uint8_t unsigned_integer = 0;
-        unsigned_integer = signed_integer;
         
-        while (1) {;}
+        while (1) {
+                ; 
+        }
         return 0;
 }
