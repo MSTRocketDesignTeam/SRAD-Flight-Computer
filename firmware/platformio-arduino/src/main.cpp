42fe8364e0004e71e90446b9a6294396ecd0ab73
Seth Sievers <ses3wn@umsystem.edu>
2024-10-18 17:10:27 -0500

    added new main file

---
+++ firmware/platformio-arduino/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <MS5611_SPI.h> // Barometer Library 
+#include <Adafruit_FRAM_SPI.h> // FRAM Library
+#include <SparkFun_KX13X.h> // High G Accelerometer Library
+#include <LoraSx1262.h> // SX1262 LORA Module Library 
+
+#include "board.h"
+
+/* ---------------------------------- SETUP --------------------------------- */
+void setup() 
+{
+        return; 
+}
+/* -------------------------------------------------------------------------- */
+
+/* ---------------------------------- LOOP ---------------------------------- */
+void loop() 
+{
+        return; 
+}
+/* -------------------------------------------------------------------------- */
